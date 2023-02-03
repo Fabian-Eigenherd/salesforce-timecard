f7b390f074f23304092b379a611f171d7c9db421
David Edwards <dedwards@bishopfox.com>
2023-02-03 11:40:39 -0600

    Finding error

--- salesforce_timecard/exceptions.py
+++ salesforce_timecard/exceptions.py
@@ -18,9 +18,13 @@ def sfdx_token_refresh(instance):
     sfdx_process = Popen([distutils.spawn.find_executable("sfdx"), 'force:org:display', '--json', '--targetusername', 'dedwards@bishopfox.com' ], stdout=PIPE, stdin=PIPE, stderr=PIPE)
     cmd_output, cmd_error = sfdx_process.communicate() 
     sfdx_results = json.loads(cmd_output.decode("utf-8")) 
-
-    username = sfdx_results['result']['username']
-    access_token = sfdx_results['result']['accessToken']
+    
+    try:
+        username = sfdx_results['result']['username']
+        access_token = sfdx_results['result']['accessToken']
+    except Exception as e:
+        print(e)
+        print(cmd_error)
     return(username,access_token)
 
 
