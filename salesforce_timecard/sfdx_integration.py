42382d0adef806672986c4a23823dd3be0b2c7b2
David Edwards <dedwards@bishopfox.com>
2023-02-13 12:42:29 -0600

    Cleanup

--- salesforce_timecard/sfdx_integration.py
+++ salesforce_timecard/sfdx_integration.py
@@ -3,7 +3,6 @@ import keyring
 from subprocess import Popen, PIPE
 import distutils.spawn
 import json
-from simple_salesforce.exceptions import SalesforceExpiredSession, SalesforceError, SalesforceAuthenticationFailed
 
 
 def manual_refresh():
@@ -16,7 +15,6 @@ def sfdx_access_token_create(username, instance):
     try:
         instanceURL = f"https://{instance}"
         sfdx_webauth_process = Popen([distutils.spawn.find_executable("sfdx"), 'auth:web:login', "-r", instanceURL], stdout=PIPE, stdin=PIPE, stderr=PIPE).communicate()
-
         sfdx_process = Popen([distutils.spawn.find_executable("sfdx"), 'force:org:display', '--json', '--targetusername', username ], stdout=PIPE, stdin=PIPE, stderr=PIPE)
         cmd_output, cmd_error = sfdx_process.communicate() 
         sfdx_results = json.loads(cmd_output.decode("utf-8")) 
